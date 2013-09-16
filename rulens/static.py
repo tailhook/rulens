52116772fccd100648cfbb040621d41bc40fb500
Paul Colomiets <paul@colomiets.name>
2013-09-16 20:03:52 +0300

    Add basic graph building

--- rulens/static.py
+++ rulens/static.py
@@ -2,8 +2,25 @@
     The command-line interface for static-checking of topology db
 """
 import argparse
+import sys
+import io
+import subprocess
+from urllib.parse import urlparse, parse_qsl
+from contextlib import contextmanager
+from collections import defaultdict
+from itertools import product
 
 from .db import Database
+
+
+@contextmanager
+def set_stdout(file):
+    oldstdout = sys.stdout
+    try:
+        sys.stdout = file
+        yield
+    finally:
+        sys.stdout = oldstdout
 
 
 def main():
@@ -39,8 +56,90 @@ def main():
 
     if options.basic_graph:
         pass
+
     if options.instance_graph:
-        pass
+        fn = 'instance_graph.png'
+        print("Writing", fn)
+        proc = subprocess.Popen(['dot', '-Tpng', '-o', fn],
+            stdin=subprocess.PIPE)
+        hosts = defaultdict(dict)
+        conn = defaultdict(list)
+        bind = defaultdict(list)
+        priorities = set()
+        counter = 0
+        for fn in options.list_file:
+            with open(fn, 'rt') as file:
+                for line in file:
+                    line = line.strip()
+                    if line.startswith('#'):  # comment
+                        continue
+                    url, socktype = line.split()
+                    u = urlparse(url)
+                    query = dict(parse_qsl(u.query))
+                    nname = '{0[hostname]}_{0[role]}_{0[pid]}'.format(query)
+                    hosts[query['hostname']][nname] = query
+                    for addr in db.resolve(None, None, url, socktype):
+                        kind, priority, raddr = addr.split(':', 2)
+                        if socktype == 'NN_REQ':  # TODO(pc) other types!
+                            priority = int(priority)
+                            priorities.add(priority)
+                        else:
+                            priority = None
+                        if kind == 'bind':
+                            bind[raddr].append((priority, nname))
+                        elif kind == 'connect':
+                            conn[raddr].append((priority, nname))
+                        else:
+                            raise AssertionError(
+                                "Wrong address {!r}".format(addr))
+        priority_styles = ['dotted', 'dashed', 'solid']
+        priority_lengths = [0, 0, 2]
+        priostyles = {None: 'solid'}
+        priolengths = {None: 1}
+        for i in sorted(priorities):
+            priostyles[i] = priority_styles.pop()
+            priolengths[i] = priority_lengths.pop()
+
+        with set_stdout(io.TextIOWrapper(proc.stdin)):
+            print("digraph topology {")
+            print("rankdir=LR")
+
+            for host, ndict in hosts.items():
+                print("subgraph cluster_{} {{".format(host))
+                print('label="{}"'.format(host))
+                for name, props in ndict.items():
+                    print('{0} [label={1[role]}]'.format(name, props))
+                print("}")
+
+            for addr, nlist in bind.items():
+                if addr in conn:
+                    for (pa, a), (pb, b) in product(nlist, conn[addr]):
+                        if pa: # priority is in a req socket
+                            prio = pa
+                            arrowhead = 'none'
+                            arrowtail = 'inv'
+                        else:
+                            prio = pb
+                            a, b = b, a
+                            arrowhead = 'normal'
+                            arrowtail = 'none'
+
+                        print('{} -> {} [style={} minlen={} arrowhead={} arrowtail={} dir=both]'
+                            .format(a, b, priostyles[prio], priolengths[prio],
+                                arrowhead, arrowtail))
+                else:
+                    for p, n in nlist:
+                        counter += 1
+                        if p is None: # priority is in a req socket
+                            print('ext_{} [shape=box label="{}" style="{}"]'
+                                .format(counter, addr, priostyles[p]))
+                            print('ext_{} -> {}'.format(counter, n))
+                        else:
+                            print('ext_{} [shape=box label="{}" style="{}" dir=back arrowhead=inv]'
+                                .format(counter, addr, priostyles[p]))
+                            print('{} -> ext_{}'.format(n, counter))
+
+            print("}")
 
 
 if __name__ == '__main__':
